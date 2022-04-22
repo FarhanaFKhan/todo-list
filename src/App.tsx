b285dcb16bb17562a117b067559f0881521c5b55
Farhana Khan <farhanafatema@hotmail.com>
2022-04-22 16:57:51 +0100

    Explore grid layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,24 @@
-import React,{FC} from 'react';
-import './App.css';
-import Header from './components/Header';
-import Container from './components/Container';
+import React, { FC } from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Container from "./components/Container";
+import { Grid, GridItem, Text } from "@chakra-ui/react";
 
 const App: FC = () => {
   return (
-    <div className="App">
-      <Header/>
-      <div>
-        <Container/>
-      </div> 
-    </div>
+    <Grid
+      templateRows="repeat(25 1fr)"
+      templateColumns="repeat(5, 1fr)"
+      gap={4}
+    >
+      <GridItem rowSpan={2} colSpan={1} bg="pink.50" />
+      <GridItem colStart={2} colEnd={5} bg="papayawhip">
+        <Header />
+      </GridItem>
+      <GridItem rowSpan={2} colSpan={1} bg="pink.50" />
+      <Container />
+    </Grid>
   );
-}
+};
 
 export default App;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { Text } from "@chakra-ui/react";
 
+// const Header = () => {
+//   return (
+//     <Grid h="80px" templateColumns="repeat(5, 1fr)" gap={4}>
+//       <GridItem colSpan={1} bg="tomato" />
+//       <GridItem colStart={2} colEnd={5} bg="papayawhip">
+//         <Text fontSize="3xl">Todo App</Text>
+//       </GridItem>
+//       <GridItem colSpan={1} bg="tomato" />
+//     </Grid>
+//   );
+// };
+// export default Header;
 const Header = () => {
-  return <div className="header">Todo App</div>;
+  return <Text fontSize="3xl">Todo App</Text>;
 };
 export default Header;
